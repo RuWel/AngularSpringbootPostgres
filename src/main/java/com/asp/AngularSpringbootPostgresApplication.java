4769239e02d0aa0399144a459bb473b87ac7584c
Rudi Welter <rudi.welter@gmail.com>
2023-12-09 05:06:14 +0100

    added initial files

---
+++ src/main/java/com/asp/AngularSpringbootPostgresApplication.java
@@ -0,0 +1,13 @@
+package com.asp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringbootPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringbootPostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asp/AngularSpringbootPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringbootPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
